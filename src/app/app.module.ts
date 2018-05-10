3b8bbd0c1595649aac791f412ebe4aabe6bb29c7
aniruddha <aniruddha.kudalkar@gmail.com>
2018-05-10 10:28:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyOneComponent } from './my-one/my-one.component';
+import { SimpleComponent } from './simple/simple.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyOneComponent,
+    SimpleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/my-one/my-one.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyOneComponent } from './my-one.component';
+
+describe('MyOneComponent', () => {
+  let component: MyOneComponent;
+  let fixture: ComponentFixture<MyOneComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MyOneComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyOneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
